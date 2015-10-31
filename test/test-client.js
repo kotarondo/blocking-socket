e3d7d65d02b013dd49ef1f07b5d680197cfe945f
Kotaro Endo <kotarondo@gmail.com>
2015-10-31 20:27:11 +0900

    tests added

---
+++ test/test-client.js
@@ -0,0 +1,59 @@
+/*
+ Copyright (c) 2015, Kotaro Endo.
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+ 
+ 1. Redistributions of source code must retain the above copyright
+    notice, this list of conditions and the following disclaimer.
+ 
+ 2. Redistributions in binary form must reproduce the above
+    copyright notice, this list of conditions and the following
+    disclaimer in the documentation and/or other materials provided
+    with the distribution.
+ 
+ 3. Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived
+    from this software without specific prior written permission.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+var bs = require('blocking-socket');
+
+var fd = bs.open("tmp/socket");
+console.log("fd="+fd);
+assert(fd > 0);
+
+var sbuf = new Buffer(500);
+for(var i=0; i<sbuf.length; i++) sbuf[i]=(i&0x7f);
+var sl = bs.send(fd, sbuf);
+console.log("sl="+sl);
+assert(sl > 0);
+for(var i=0; i<sbuf.length; i++) assert(sbuf[i]===(i&0x7f));
+
+var rbuf = new Buffer(500);
+var rl = bs.recv(fd, rbuf);
+console.log("rl="+rl);
+assert(rl > 0);
+for(var i=0; i<rl; i++) assert(rbuf[i]===(i&0x7f));
+
+bs.close(fd);
+console.log("ok");
+process.exit(0);
+
+function assert(cond){
+	if(!cond) throw new Error("assertion");
+}
