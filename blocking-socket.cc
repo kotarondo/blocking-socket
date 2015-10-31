588566261a5cc72007afb20b99cd2075c72943f6
Kotaro Endo <kotarondo@gmail.com>
2015-10-31 17:34:50 +0900

    interface created

--- blocking-socket.cc
+++ blocking-socket.cc
@@ -32,23 +32,80 @@
 */
 
 #include <node.h>
+#include <node_buffer.h>
 
 namespace blocking_socket {
 
 using v8::FunctionCallbackInfo;
 using v8::Isolate;
 using v8::Local;
-using v8::Object;
+using v8::Number;
 using v8::String;
+using v8::Object;
 using v8::Value;
+using v8::Exception;
 
-void Method(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = args.GetIsolate();
-  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
+int openBody(const char* name, size_t size) {
+	return size;
+}
+
+int closeBody(int fd) {
+	return fd;
+}
+
+int sendBody(int fd, const char* buf, size_t size) {
+	return size;
+}
+
+int recvBody(int fd, char* buf, size_t size) {
+	return size;
+}
+
+void openMethod(const FunctionCallbackInfo<Value>& args) {
+	Isolate* isolate = args.GetIsolate();
+	String::Utf8Value name(args[0]->ToString());
+	int fd = openBody(*name, name.length());
+	args.GetReturnValue().Set(Number::New(isolate, fd));
+}
+
+void closeMethod(const FunctionCallbackInfo<Value>& args) {
+	Isolate* isolate = args.GetIsolate();
+	int fd = args[0]->ToInteger()->NumberValue();
+	int err = closeBody(fd);
+	args.GetReturnValue().Set(Number::New(isolate, err));
+}
+
+void sendMethod(const FunctionCallbackInfo<Value>& args) {
+	Isolate* isolate = args.GetIsolate();
+	if (args.Length() < 2 || !node::Buffer::HasInstance(args[1])) {
+		isolate->ThrowException(
+				Exception::TypeError(
+						String::NewFromUtf8(isolate, "Wrong arguments")));
+		return;
+	}
+	int fd = args[0]->ToInteger()->NumberValue();
+	int len = sendBody(fd, node::Buffer::Data(args[1]), node::Buffer::Length(args[1]));
+	args.GetReturnValue().Set(Number::New(isolate, len));
+}
+
+void recvMethod(const FunctionCallbackInfo<Value>& args) {
+	Isolate* isolate = args.GetIsolate();
+	if (args.Length() < 2 || !node::Buffer::HasInstance(args[1])) {
+		isolate->ThrowException(
+				Exception::TypeError(
+						String::NewFromUtf8(isolate, "Wrong arguments")));
+		return;
+	}
+	int fd = args[0]->ToInteger()->NumberValue();
+	int len = recvBody(fd, node::Buffer::Data(args[1]), node::Buffer::Length(args[1]));
+	args.GetReturnValue().Set(Number::New(isolate, len));
 }
 
 void init(Local<Object> exports) {
-  NODE_SET_METHOD(exports, "open", Method);
+	NODE_SET_METHOD(exports, "open", openMethod);
+	NODE_SET_METHOD(exports, "close", closeMethod);
+	NODE_SET_METHOD(exports, "send", sendMethod);
+	NODE_SET_METHOD(exports, "recv", recvMethod);
 }
 
 NODE_MODULE(addon, init)
